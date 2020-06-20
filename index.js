a2e4bf6b9610cd15780b64e34f5e6373ed7ba655
amir0900 <amir0900@gmail.com>
2020-06-21 00:31:44 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+function addUpTo(n){
+  const total;
+  for(let i = 0; i < n; i++){
+    total += i;
+  }
+  return total;
+}
 
-import './big-o-notation/addupto';
+const t1 = performance.now()
+addUpTo(6);
+const t2 = performance.now();
+console.log(`time elapsed in ${(t2 - t1) / 1000} seconds`)
