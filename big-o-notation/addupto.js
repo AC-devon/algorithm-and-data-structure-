66071f97268b0b8c02f769af0df9941e51bbfc4b
amir0900 <amir0900@gmail.com>
2020-06-21 01:22:34 +0300

    2 files modified

--- big-o-notation/addupto.js
+++ big-o-notation/addupto.js
@@ -1,3 +1,6 @@
+// note: determine big o complexity above each fn
+
+// O(n)
 function addUpToLoop(n) {
   const total = 0;
   for (let i = 0; i <= n; i++) {
@@ -6,9 +9,21 @@ function addUpToLoop(n) {
   return total;
 }
 
+//O(1)
 function addUpToFormula(n) {
   // zomg wut formula
   return (n * (n + 1)) / 2;
+}
+
+// O(n^2)
+function addUpNestLoop(n) {
+  const total = 0;
+  for (let i = 0; i <= n; i++) {
+    for (let j = i; j <= i; j++) {
+      total += j;
+    }
+  }
+  return total;
 }
 
 function timeCalc(num, cb) {
@@ -18,9 +33,11 @@ function timeCalc(num, cb) {
   console.log(`time elapsed in ${(t2 - t1) / 1000} seconds`);
 }
 
-timeCalc(1000000000, addUpToFormula);
+timeCalc(100, addUpToFormula);
 
 /* conclusion 
   1. time space cannot be a good measurement of efficiency between 2 algorithms since the margin is diffrent between computers/conditions
   2. the big O relay of the amount of operations and not size of N
+  3. big O allow us to roughly calculate how the runtime of an algorithm grows as the inputs grow 
+  4, eventullay we need to think of best algorithm 
 */
