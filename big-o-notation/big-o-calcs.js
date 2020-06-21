a7bb2083e3c72ded2bb638d55afaf39e3def0ade
amir0900 <amir0900@gmail.com>
2020-06-21 08:28:49 +0300

    1 file added

---
+++ big-o-notation/big-o-calcs.js
@@ -0,0 +1,10 @@
+// count the amount of operations in a function
+function countOperations(fn){
+
+}
+
+// count the amount of constant in a function
+function countConstant(fn){}
+
+// get fn and complexity type
+function defineBigO(){}
